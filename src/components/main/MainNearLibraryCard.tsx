7c8304b2bb54ba440396ae4e50a2bc4d4c32c007
Jinkyum Park <me@jinkyumpark.com>
2024-02-11 16:53:35 +0900

    [feat] Main 내 주변 도서관 더 보기 버튼 추가

--- src/components/main/MainNearLibraryCard.tsx
+++ src/components/main/MainNearLibraryCard.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import LibraryNearCard from '../library/near/LibraryNearCard'
+import AllButton from '../common/AllButton'
 
 const MainNearLibraryCard = () => {
 	return (
 		<div className='h-100' style={{ minHeight: '525px' }}>
 			<a href='/library/membership/all' className='text-black h-100'>
-				<LibraryNearCard col='col-12' moreButton={false} size={8} mt={3} />
+				<LibraryNearCard moreButton={true} size={8} mt={3} />
 			</a>
 		</div>
 	)
